f39660d8442ffeaee32b444bd722634017dcef23
amandalyckenius <alyckenius@gmail.com>
2025-06-26 01:56:57 +0200

    Initial commit

---
+++ src/main/java/org/amanda/timecapsule/TimeCapsule.java
@@ -0,0 +1,8 @@
+package org.amanda.timecapsule;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class TimeCapsule {
+}
---
+++ src/test/java/org/amanda/timecapsule/TimeCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amanda.timecapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCapsuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
