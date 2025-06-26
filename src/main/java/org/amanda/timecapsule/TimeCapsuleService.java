0126be94525f641434b62e0bc9b3472e90f1bdd6
amandalyckenius <alyckenius@gmail.com>
2025-06-26 03:05:40 +0200

    Add: logic to create capsule in service

--- src/main/java/org/amanda/timecapsule/TimeCapsuleService.java
+++ src/main/java/org/amanda/timecapsule/TimeCapsuleService.java
@@ -1,6 +1,9 @@
 package org.amanda.timecapsule;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TimeCapsuleService {
@@ -8,5 +11,16 @@ public class TimeCapsuleService {
 
     public TimeCapsuleService(TimeCapsuleRepository timeCapsuleRepository) {
         this.timeCapsuleRepository = timeCapsuleRepository;
+    }
+
+    public void createCapsule(@Valid TimeCapsuleRequest timeCapsuleRequest) {
+
+        TimeCapsule capsule = new TimeCapsule();
+        capsule.setEmail(timeCapsuleRequest.getEmail());
+        capsule.setMessage(timeCapsuleRequest.getMessage());
+        capsule.setDeliveryDate(timeCapsuleRequest.getDeliveryDate());
+        capsule.setCreatedAt(LocalDateTime.now());
+
+        timeCapsuleRepository.save(capsule);
     }
 }
