1ed45566b7c7ac667111b6118211f2565fdcc58c
amandalyckenius <alyckenius@gmail.com>
2025-06-26 02:57:37 +0200

    Add: PostMapping method createCapsule in controller

--- src/main/java/org/amanda/timecapsule/TimeCapsuleController.java
+++ src/main/java/org/amanda/timecapsule/TimeCapsuleController.java
@@ -1,5 +1,9 @@
 package org.amanda.timecapsule;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +14,11 @@ public class TimeCapsuleController {
 
     public TimeCapsuleController(TimeCapsuleService timeCapsuleService) {
         this.timeCapsuleService = timeCapsuleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCapsule(@Valid @RequestBody TimeCapsuleRequest timeCapsuleRequest){
+        timeCapsuleService.createCapsule(timeCapsuleRequest);
+        return ResponseEntity.ok("Time capsule has been successfully created!")
     }
 }
