dfe9e7d96a8b32af18e94a64862f172ed7451a58
amandalyckenius <alyckenius@gmail.com>
2025-10-07 14:24:59 +0200

    Refactor: jpaRepository to MongoRepostitory
    Refactor: pom.xml with correct dependencies. Add: try catch to createCapsule in controller. Delete: dockerfile.

--- src/main/java/org/amanda/timecapsule/TimeCapsuleRepository.java
+++ src/main/java/org/amanda/timecapsule/TimeCapsuleRepository.java
@@ -1,10 +1,9 @@
 package org.amanda.timecapsule;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.mongodb.repository.MongoRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface TimeCapsuleRepository extends JpaRepository <TimeCapsule, Long> {
+public interface TimeCapsuleRepository extends MongoRepository<TimeCapsule, String> {
     public List<TimeCapsule> findByDeliveryDateAndSentFalse(LocalDate deliveryDate);
 }
