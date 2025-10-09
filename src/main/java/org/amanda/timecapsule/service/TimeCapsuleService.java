3884efe072535bc4470b2620ad1839313f1b2cf5
amandalyckenius <alyckenius@gmail.com>
2025-10-09 10:21:44 +0200

    refactor: deliverTodaysCapsules into TimeCapsuleService
    delete: timeCapsuleScheduler class and package

--- src/main/java/org/amanda/timecapsule/service/TimeCapsuleService.java
+++ src/main/java/org/amanda/timecapsule/service/TimeCapsuleService.java
@@ -1,19 +1,24 @@
 package org.amanda.timecapsule.service;
 
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.amanda.timecapsule.model.TimeCapsule;
 import org.amanda.timecapsule.repository.TimeCapsuleRepository;
 import org.amanda.timecapsule.model.TimeCapsuleRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class TimeCapsuleService {
     private final TimeCapsuleRepository timeCapsuleRepository;
+    private final MailService mailService;
 
-    public TimeCapsuleService(TimeCapsuleRepository timeCapsuleRepository) {
+    public TimeCapsuleService(TimeCapsuleRepository timeCapsuleRepository, MailService mailService) {
         this.timeCapsuleRepository = timeCapsuleRepository;
+        this.mailService = mailService;
     }
 
     public TimeCapsule createCapsule(@Valid TimeCapsuleRequest timeCapsuleRequest) {
@@ -27,5 +32,26 @@ public class TimeCapsuleService {
         timeCapsuleRepository.save(capsule);
 
         return capsule;
+    }
+
+    public int deliverTodaysCapsules() {
+        LocalDate today = LocalDate.now();
+        List<TimeCapsule> timeCapsulesDue = timeCapsuleRepository.findByDeliveryDateAndSentFalse(today);
+        int sentCount = 0;
+
+        for(TimeCapsule capsule: timeCapsulesDue){
+
+            try {
+                capsule.setSent(true);
+                timeCapsuleRepository.save(capsule);
+                mailService.sendCapsuleMail(capsule);
+
+                sentCount++;
+            } catch (Exception e){
+                capsule.setSent(false);
+                timeCapsuleRepository.save(capsule);
+            }
+        }
+        return sentCount;
     }
 }
